885d3f52c4b7c24f77ed7e59699561323c79a9bd
Julie Langmade <jlangmade@gmail.com>
2015-07-09 15:39:37 -0500

    completed main.js file

---
+++ README.md
@@ -0,0 +1,11 @@
+# JavaScript Introduction
+***
+
+### due by 8am, on 7/10/15
+
+***
+
+## Contents
+***
+1. README.md
+2. Completed main.js
---
+++ main.js
@@ -0,0 +1,46 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+// undefined
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// Tim
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// Hello, how are you?
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// 45
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+// var age = today - born;
+//var result("Tim is " + age + " years old");
+
+
+// Store some information following in variables.
+var yourName = 'Julie';
+var instructorName = 'Will';
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+// var yourName = 'Julie';
+// var instructorName = 'Will';
+// var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+// statement
+// 'Julie is taking a class at The Iron Yard, my instructor\'s name is Will'
